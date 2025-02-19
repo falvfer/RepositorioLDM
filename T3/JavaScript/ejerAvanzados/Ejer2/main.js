e1fd20e35c201c2e0a2fb6b9e1cc1127e2e39441
falvfer <falvfer2612@g.educaand.es>
2025-02-19 14:56:59 +0100

    V250219-2_Clase

---
+++ T3/JavaScript/ejerAvanzados/Ejer2/main.js
@@ -0,0 +1,7 @@
+function convertir() {
+    celcius = Number.parseFloat(document.getElementById("celcius").value);
+    fahrenheit = document.getElementById("fahrenheit");
+
+    if (!isNaN(celcius))
+        fahrenheit.value = (celcius*9/5+32);
+}
